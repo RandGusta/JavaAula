5babea38f3f4ce8970c7fd1e35208008a7a27179
RandGusta <gustarandiiii@gmail.com>
2025-05-09 14:54:31 -0300

    Update MergeSort.java

--- Primeiro_Trimestre/MergeSort.java
+++ Primeiro_Trimestre/MergeSort.java
@@ -1,43 +1,65 @@
-# JavaAula
+public class Main{
+	public static void main(String[] args){
 
-public Main{
-  public static void main(String[] args){
-
-   private static void merge(int[] vetor, int[] aux, int inicio, int fim){
-      int meio = (inicio + fim)/2;
-      if(inicio<fim){
-        merge(vetor, aux, inicio, meio); // chama a função novamente 
-        merge(vetor, aux, meio+1, fim);
-        intercalacao(vetor, aux, inicio, meio, fim);
-      }
-     
-   }
-
-public static void intercalacao(int[] vetor, int[] aux, int inicio, int meio, int fim){
-    for(int i = inicio; i<=fim; i++){
-        aux[i] = vetor[i];
-      
-    }
-    int esquerda = inicio;
-    int direita = meio+1; // o meio ainda faz parte do inicio então é meio+1;
-
-    for(int i=inicio; i<=fim; i++){
-      if(inicio > meio){
-        vetor[i] = aux[direita++];
-      } else if(direita > fim){ 
-        vetor[i] = aux[esquerda++]; //PODE ESTAR ERRADO!!!!!!
-      }
-      else if(aux[esquerda]<aux[direita]){
-        vetor[i] = aux[esquerda++];
-      } else{
-        vetor[i] = aux[direita++];
-      }
-    }
-}
+    int[] vetor = {1223,343, 11, 53, -1};
+    mergeSortOrdenaar();
+    for(int k=0; k<vetor.length; k++){
+        System.out.println(veto[k]);
     
-    
-  }
-
-
+    }
   
+
+
+
+	}
+
+	public static void mergeSortDividir(int[] vetor, int inicio, int fim){ // ele é responsável por dividir o vetor até chegar em vetores de 1 numero
+		int meio = (inicio + fim)/2;
+		mergeSortDividir(vetor, inicio, meio); // chamando ele para continuat dividindo a parte esquerda
+		mergeSortDividir(vetor, meio+1, fim); // chamando ele para continuat dividindo a parte direita
+		mergeSortOrdenaar(vetor, inicio, meio, fim);
+
+	}
+
+	public static void mergeSortOrdenaar(int[] vetor, int inicio, int meio, int fim){ // responsável por juntar os vetores unitários
+	int[] vetorTemporario = new int[fim - inicio + 1];
+	int i = inicio; //inicio da parte esquerda
+	int j = meio + 1; //inicio da parte direita
+	int k = 0; //índice de vetorTemporario
+
+	while(i <= meio && j <= fim){
+		if(vetor[i] < vetor[j]){
+		vetorTemporario[k++] = vetor[i++];
+		} else {
+		vetorTemporario[k++] = vetor[j++];
+		}
+	}
+
+	
+	
+	while(i<=meio){
+	vetorTemporario[k++] = vetor[i++];
+
+	}
+
+	while(j<=fim){
+	vetorTemporario[k++] = vetor[j++];
+	}
+
+	for(i = 0; i<vetorTemporario.length; i++){
+		vetor[i+inicio] = vetorTemporario[i];
+
+	}
+
+
+
+
+
+	}
+
+
+
+
+
+
 }
