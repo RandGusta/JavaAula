f4a8058f7e3105b8ea216168be0e0231fd4bc8b1
RandGusta <gustarandiiii@gmail.com>
2025-05-29 13:19:41 -0300

    Update MergeSort.java

--- Primeiro_Trimestre/MergeSort.java
+++ Primeiro_Trimestre/MergeSort.java
@@ -14,11 +14,12 @@ public class Main{
 	}
 
 	public static void mergeSortDividir(int[] vetor, int inicio, int fim){ // ele é responsável por dividir o vetor até chegar em vetores de 1 numero
+		if(inicio<fim){
 		int meio = (inicio + fim)/2;
 		mergeSortDividir(vetor, inicio, meio); // chamando ele para continuat dividindo a parte esquerda
 		mergeSortDividir(vetor, meio+1, fim); // chamando ele para continuat dividindo a parte direita
 		mergeSortOrdenaar(vetor, inicio, meio, fim);
-
+		}
 	}
 
 	public static void mergeSortOrdenaar(int[] vetor, int inicio, int meio, int fim){ // responsável por juntar os vetores unitários
