b503cb7db30282a6c2cd9244c93039fe5a97a2ee
RandGusta <gustarandiiii@gmail.com>
2025-06-06 22:35:00 -0300

    Update MainListaEncadeada.java

--- Segundo_Trimestre/Aula06_06/MainListaEncadeada.java
+++ Segundo_Trimestre/Aula06_06/MainListaEncadeada.java
@@ -1,1 +1,22 @@
+// linked list
+// composta por nós com as informações básicas de valor dele (int, string...) e ponteiro (Aponta para o próximo elemento --> referencia para o próximo elemento [ outro elemento do mesmo tipo do nó])
+// ultimo nó aponta para o nulo
+// Cabeça --> inicio da lista
+// Ex. para adicionar um elemento no INICIO da lista --> fazer com que esse novo elemento aponte
+// para onde a 'cabeça' esta apontando
+// Ex. para adicionar um elemento no FIM da lista --> acesso o incio, percorro toda a lista, quando achar o elemento
+// que aponta para NULO faço ele apontar para esse novo elemento e agora esse novo é o ultimo
+// ele aponta para NULO
+// INTERESSANTE --> armazenar quem é o ultimo para não termos que percorrer
 
+public class Main{
+    public static void main(String[] args){
+        ListaEncadeada lista = new ListaEncadeada();
+        lista.adicionarInicio(10);
+        lista.adicionarInicio(5);
+        lista.imprimir();
+        lista.adicionarFim(56);
+        lista.imprimir();
+    }
+
+}
