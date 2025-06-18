a8d5eff3b40d4dd362a880321a8f3e5f62c15282
RandGusta <gustarandiiii@gmail.com>
2025-06-18 14:17:50 -0300

    Update LinkedList.java

--- Segundo_Trimestre/ExerciciosListaEncadeadaSimples/Exercicio02/LinkedList.java
+++ Segundo_Trimestre/ExerciciosListaEncadeadaSimples/Exercicio02/LinkedList.java
@@ -6,3 +6,97 @@
 // Implemente um método que inverta a lista encadeada.
 // Implemente uma função que retorne o k-ésimo elemento da lista (base 0).
 
+
+import java.sql.SQLOutput;
+
+public class LikedList<T> {
+    private Node<T> cabeca;
+
+
+    public void inserirInicio(T valor) {
+        Node<T> elemento = new Node<>(valor);
+        if (cabeca == null) {
+            cabeca = elemento;
+
+        } else {
+            elemento.proximo = cabeca;
+            cabeca = elemento;
+
+
+        }
+
+
+    }
+
+
+    public void inserirFinal(T valor) {
+        Node<T> elemento = new Node <>(valor);
+        Node<T> temp = cabeca;
+        if (cabeca == null) {
+            cabeca = elemento;
+
+        } else {
+            while (temp.proximo != null) {
+                temp = temp.proximo;
+            }
+            temp.proximo = elemento;
+
+
+        }
+
+
+    }
+
+
+    public void imprimir() {
+        Node<T> temp = cabeca;
+        while (temp != null) {
+            System.out.print(temp.valor + " -->");
+            temp = temp.proximo;
+        }
+        System.out.print("NULL");
+        System.out.println();
+
+    }
+
+
+    public void contarElementos() {
+        Node<T> temp = cabeca;
+        int cont = 0;
+        while (temp != null) {
+            cont++;
+            temp = temp.proximo;
+        }
+        System.out.println("Essa lista possui : " + cont + " elemento(s)");
+
+    }
+
+
+    public void removerElementoInicio(){
+        if(cabeca == null){
+            System.out.println("Lista já não possui elementos");
+        } else {
+            cabeca = cabeca.proximo;
+        }
+
+
+    }
+
+
+
+
+    public void removerElementoFim(){
+        if(cabeca == null){
+            System.out.println("A lista está vazia!!!");
+        }else if(cabeca.proximo == null) {
+            cabeca = null;
+        } else {
+            Node<T> temp = cabeca;
+            while((temp.proximo).proximo != null){
+                    temp = temp.proximo;
+            }
+            temp.proximo = null;
+        }
+    }
+
+}
