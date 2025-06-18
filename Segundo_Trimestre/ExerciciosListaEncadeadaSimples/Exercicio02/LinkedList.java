4f769d6041bfbd978ee7dd0a9ea11337f6879917
RandGusta <gustarandiiii@gmail.com>
2025-06-18 14:17:06 -0300

    Update and rename blbls.md to LinkedList.java

---
+++ Segundo_Trimestre/ExerciciosListaEncadeadaSimples/Exercicio02/LinkedList.java
@@ -0,0 +1,8 @@
+// Nível 2 – Intermediário: Inserções e Remoções por Posição
+// Implemente um método que insira um elemento em uma posição específica da 
+// lista - por índice e após um valor específico.
+// Implemente um método que remova um elemento de uma posição específica da lista.
+// Crie um método que remova todos os elementos com um valor específico.
+// Implemente um método que inverta a lista encadeada.
+// Implemente uma função que retorne o k-ésimo elemento da lista (base 0).
+
