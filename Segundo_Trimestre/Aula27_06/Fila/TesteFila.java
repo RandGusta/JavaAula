72e99f9ea61602e372195d1c3a432fc2cf841cca
RandGusta <gustarandiiii@gmail.com>
2025-06-27 22:02:11 -0300

    Create TesteFila.java

---
+++ Segundo_Trimestre/Aula27_06/Fila/TesteFila.java
@@ -0,0 +1,10 @@
+public class TesteFila{
+  public static void main(String[] args){
+  Fila<String> pessoas = new Fila<>();
+    pessoas.enfileirar("João");
+    pessoas.enfilieirar("Fabio");
+    pessoas.desenfileirar();
+    
+  }
+  
+}
